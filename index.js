68f5ed8b5c8766d971ed3465569f4b37e369040f
frustrated-coder <d.n_mwangi@outlook.com>
2022-08-02 19:28:51 +0300

    added all maths fns as required

---
+++ index.js
@@ -0,0 +1,29 @@
+//define these fns add(), subtract(), multiply(), and divide()
+
+function add(a , b){
+    return a + b;
+}
+
+function subtract(a, b){
+    return a -b;
+}
+function multiply(a, b){
+    return b * a;
+}
+function divide(a , b){
+    return a / b
+}
+function increment(a){
+    return ++a
+}
+function decrement(b){
+    return --b
+}
+
+function makeInt(n){
+    return parseInt(n, 10)
+
+}
+function preserveDecimal(n){
+    return parseFloat(n)
+}
